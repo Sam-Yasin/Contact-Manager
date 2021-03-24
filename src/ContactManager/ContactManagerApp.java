2b5725719a96a42b8c1b63477b3073600933cb09
Yasin Mogultay <yasin.mglty@gmail.com>
2021-03-23 20:46:20 -0500

    Completed Contact Manager App

--- src/ContactManager/ContactManagerApp.java
+++ src/ContactManager/ContactManagerApp.java
@@ -1,5 +1,4 @@
 package ContactManager;
-
 import java.util.Scanner;
 
 public class ContactManagerApp {
@@ -7,33 +6,9 @@ public class ContactManagerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("***Welcome to Contacts Manager***\n");
-        System.out.println(ContactTools.returnMenuDisplay());
+        System.out.println(ContactTools.menuDisplay());
         int userInput = scanner.nextInt();
-//        ContactTools.viewContacts();
-//        ContactTools.addNewContact();
-//        ContactTools.searchContact();
-//        ContactTools.deleteExistingContact();
-        runAllMethod(userInput);
-
-
-
+        ContactTools.executeChoice(userInput);
     }
 
-    public static void runAllMethod(int choice) {
-        switch (choice) {
-            case 1:
-                ContactTools.viewContacts();
-                break;
-            case 2:
-                ContactTools.addNewContact();
-                break;
-            case 3:
-                ContactTools.searchContact();
-            case 4:
-                ContactTools.deleteExistingContact();
-            default:
-                System.out.println("GoodBye!");
-                break;
-        }
-    }
 }
--- src/ContactManager/ContactTools.java
+++ src/ContactManager/ContactTools.java
@@ -12,21 +12,37 @@ import java.util.Scanner;
 public class ContactTools {
     private static final Path cmp = Paths.get("src", "ContactManager", "peopleContacts.txt");
 
+    //Prompting user for a recursion.
+    private static void promptUserForChoice() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Do you want to continue (y/n)\n");
+        String userInput = sc.nextLine();
+        if (userInput.equalsIgnoreCase("y")) {
+            System.out.println(menuDisplay());
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter Your Choice!");
+            int userInput1 = scanner.nextInt();
+            System.out.println(executeChoice(userInput1));
+        } else {
+            System.out.println("* * * Thanks for using! * * *");
+        }
+    }
 
 
-
-    public static String returnMenuDisplay() {
-        String choice = "Would you like to?\n\n" +
+    //Displaying main menu
+    public static String menuDisplay() {
+        return "Would you like to?\n\n" +
                 "1. View contacts.\n" +
                 "2. Add a new contact.\n" +
                 "3. Search contacts by name.\n" +
                 "4. Delete Existing contact.\n" +
                 "5. Exit" +
                 "\n\n" +
-                "Select 1, 2, 3, 4, or 5\n";
-        return choice;
+                "Select 1, 2, 3, 4, or 5";
     }
 
+
+    //Iterating through .txt file and returning all contacts
     public static void viewContacts() {
         List<String> contacts = new ArrayList<>();
 
@@ -44,6 +60,7 @@ public class ContactTools {
         System.out.println("--------------------------------------------");
     }
 
+    //Taking user input and adding it to .txt file
     public static void addNewContact() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name and number");
@@ -57,6 +74,8 @@ public class ContactTools {
         }
     }
 
+
+    //Taking user input and adding it to the existing ArrayList and then displaying it
     public static void searchContact() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a name");
@@ -82,6 +101,8 @@ public class ContactTools {
         }
     }
 
+
+    //According to user input deleting existing data
     public static void deleteExistingContact() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a name that you want to delete");
@@ -94,7 +115,7 @@ public class ContactTools {
             e.printStackTrace();
         }
         String userInput = sc.nextLine();
-        for(String contact : contacts) {
+        for (String contact : contacts) {
             if (contact.contains(userInput)) {
                 contactsToDelete.add(contact);
                 System.out.println("Contact has been deleted");
@@ -106,9 +127,39 @@ public class ContactTools {
         contacts.removeAll(contactsToDelete);
         try {
             Files.write(cmp, contacts);
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    //User selections and recursion for all above
+    public static boolean executeChoice(int choice) {
+        boolean keepRunning = true;
+        switch (choice) {
+            case 1:
+                viewContacts();
+                promptUserForChoice();
+                break;
+            case 2:
+                addNewContact();
+                promptUserForChoice();
+                break;
+            case 3:
+                searchContact();
+                promptUserForChoice();
+                break;
+            case 4:
+                deleteExistingContact();
+                promptUserForChoice();
+                break;
+            case 5:
+                System.out.println("* * * Thanks for using! * * *");
+                keepRunning = false;
+                break;
+
+        }
+        return keepRunning;
     }
 
 }
